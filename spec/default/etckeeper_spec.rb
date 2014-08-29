a1c55a21c3033d07fc25b07dd9022087bc595888
Mark Kusch <mark.kusch@gmail.com>
2014-08-29 10:02:48 +0200

    Add integration tests for etckeeper

--- spec/default/etckeeper_spec.rb
+++ spec/default/etckeeper_spec.rb
@@ -1,3 +1,20 @@
 require 'spec_helper'
 
 # write up RSpec integration tests here
+%w{
+  git
+  etckeeper
+}.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe file('/etc/etckeeper') do
+  it { should be_directory }
+end
+
+describe file('/etc/etckeeper/etckeeper.conf') do
+  it { should be_file }
+  its(:content) { should match /VCS="git"/ }
+end
